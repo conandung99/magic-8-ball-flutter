2cb7fad248bbdcad5c212040572393ba046f467e
dungbk <tiendungvk.0895@gmail.com>
2020-12-27 22:40:25 +0700

    Create a Statefull Widget [68]

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,20 @@ void main() => runApp(
       ),
     );
 
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('images/ball1.png'),
+    );
+  }
+}
+
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -15,7 +29,7 @@ class BallPage extends StatelessWidget {
         title: Text('Ask Me Anything'),
       ),
       backgroundColor: Colors.blue,
-      body: Container(),
+      body: Ball(),
     );
   }
 }
